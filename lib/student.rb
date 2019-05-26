9ecaa8f77aa1373609a1400ad2970856dd9ac20a
Tim Knoll <timothy.knoll@gmail.com>
2019-05-26 17:38:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,8 @@ class Student
   def self.first_X_students_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade IS NOT 12
+      WHERE grade = 10 
+      LIMIT = ?
     SQL
     
     DB[:conn].execute(sql).map do |row|
