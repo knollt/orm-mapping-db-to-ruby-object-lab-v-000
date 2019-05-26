3cc9033a6fc6aa5a0eafdeb079b74fa569748781
Tim Knoll <timothy.knoll@gmail.com>
2019-05-26 17:28:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,10 @@ class Student
       SELECT * FROM
       WHERE grade = 9 
     SQL
+    
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+    end      
   end   
   
   def save
