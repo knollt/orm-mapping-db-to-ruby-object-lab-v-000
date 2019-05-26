ac9062dc21aa82e2c2fdfe6fb1e774c1cdc5f493
Tim Knoll <timothy.knoll@gmail.com>
2019-05-26 17:38:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     end      
   end  
   
-  def self.students_below_12th_grade 
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade IS NOT 12
