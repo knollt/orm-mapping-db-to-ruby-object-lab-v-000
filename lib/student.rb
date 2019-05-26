ef065bbaf615d865ab308d326f02065f25b25c78
Tim Knoll <timothy.knoll@gmail.com>
2019-05-26 17:36:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   def self.students_below_12th_grade 
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 9 
+      WHERE grade IS NOT 12
     SQL
     
     DB[:conn].execute(sql).map do |row|
